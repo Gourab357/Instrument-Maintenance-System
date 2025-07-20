3b64384bb0fa2e6b34947c10cdc4fa796279c98c
Gourab Mahato <78420677+Gourab357@users.noreply.github.com>
2025-07-21 00:43:55 +0530

    Add files via upload

---
+++ Instrument Maintenance System/Workspace/GOURAB357.sql
@@ -0,0 +1,257 @@
+prompt --application/set_environment
+set define off verify off feedback off
+whenever sqlerror exit sql.sqlcode rollback
+--------------------------------------------------------------------------------
+--
+-- Oracle APEX export file
+--
+-- You should run this script using a SQL client connected to the database as
+-- the owner (parsing schema) of the application or as a database user with the
+-- APEX_ADMINISTRATOR_ROLE role.
+--
+-- This export file has been automatically generated. Modifying this file is not
+-- supported by Oracle and can lead to unexpected application and/or instance
+-- behavior now or in the future.
+--
+-- NOTE: Calls to apex_application_install override the defaults below.
+--
+--------------------------------------------------------------------------------
+begin
+wwv_flow_imp.import_begin (
+ p_version_yyyy_mm_dd=>'2024.05.31'
+,p_default_workspace_id=>14140705628718478115
+);
+end;
+/
+prompt  WORKSPACE 14140705628718478115
+--
+-- Workspace, User Group, User, and Team Development Export:
+--   Date and Time:   11:30 Friday August 9, 2024
+--   Exported By:     GOURABMAHATO009@GMAIL.COM
+--   Export Type:     Workspace Export
+--   Version:         24.1.3
+--   Instance ID:     63113759365424
+--
+-- Import:
+--   Using Instance Administration / Manage Workspaces
+--   or
+--   Using SQL*Plus as the Oracle user APEX_240100
+ 
+begin
+    wwv_flow_imp.set_security_group_id(p_security_group_id=>14140705628718478115);
+end;
+/
+----------------
+-- W O R K S P A C E
+-- Creating a workspace will not create database schemas or objects.
+-- This API creates only the meta data for this APEX workspace
+prompt  Creating workspace GOURAB357...
+begin
+wwv_flow_fnd_user_api.create_company (
+  p_id => 14141507638757743345
+ ,p_provisioning_company_id => 14140705628718478115
+ ,p_short_name => 'GOURAB357'
+ ,p_display_name => 'GOURAB357'
+ ,p_first_schema_provisioned => 'WKSP_GOURAB357'
+ ,p_company_schemas => 'WKSP_GOURAB357'
+ ,p_account_status => 'ASSIGNED'
+ ,p_allow_plsql_editing => 'Y'
+ ,p_allow_app_building_yn => 'Y'
+ ,p_allow_packaged_app_ins_yn => 'Y'
+ ,p_allow_sql_workshop_yn => 'Y'
+ ,p_allow_team_development_yn => 'Y'
+ ,p_allow_to_be_purged_yn => 'Y'
+ ,p_allow_restful_services_yn => 'Y'
+ ,p_source_identifier => 'GOURAB35'
+ ,p_webservice_logging_yn => 'Y'
+ ,p_path_prefix => 'GOURAB357'
+ ,p_files_version => 1
+ ,p_is_extension_yn => 'N'
+ ,p_env_banner_yn => 'N'
+ ,p_env_banner_pos => 'LEFT'
+);
+end;
+/
+----------------
+-- G R O U P S
+--
+prompt  Creating Groups...
+begin
+wwv_flow_fnd_user_api.create_user_group (
+  p_id => 1815043986472711,
+  p_GROUP_NAME => 'OAuth2 Client Developer',
+  p_SECURITY_GROUP_ID => 10,
+  p_GROUP_DESC => 'Users authorized to register OAuth2 Client Applications');
+end;
+/
+begin
+wwv_flow_fnd_user_api.create_user_group (
+  p_id => 1814950743472711,
+  p_GROUP_NAME => 'RESTful Services',
+  p_SECURITY_GROUP_ID => 10,
+  p_GROUP_DESC => 'Users authorized to use RESTful Services with this workspace');
+end;
+/
+begin
+wwv_flow_fnd_user_api.create_user_group (
+  p_id => 1814853097472710,
+  p_GROUP_NAME => 'SQL Developer',
+  p_SECURITY_GROUP_ID => 10,
+  p_GROUP_DESC => 'Users authorized to use SQL Developer with this workspace');
+end;
+/
+prompt  Creating group grants...
+----------------
+-- U S E R S
+-- User repository for use with APEX cookie-based authentication.
+--
+prompt  Creating Users...
+begin
+wwv_flow_fnd_user_api.create_fnd_user (
+  p_user_id                      => '14140705555694478115',
+  p_user_name                    => 'GOURABMAHATO009@GMAIL.COM',
+  p_first_name                   => 'Gourab',
+  p_last_name                    => 'Mahato',
+  p_description                  => '',
+  p_email_address                => 'gourabmahato009@gmail.com',
+  p_web_password                 => '6E7024CF6E97992C8C0B81E91315B925326D33453414E2E022785D2BDB59E9173D46B75B3C634943ED46D897C58A045C23446A99A4352F0BB1D8B36241BA7ACD',
+  p_web_password_format          => '5;5;10000',
+  p_group_ids                    => '',
+  p_developer_privs              => 'ADMIN:CREATE:DATA_LOADER:EDIT:HELP:MONITOR:SQL',
+  p_default_schema               => 'WKSP_GOURAB357',
+  p_account_locked               => 'N',
+  p_account_expiry               => to_date('202407161558','YYYYMMDDHH24MI'),
+  p_failed_access_attempts       => 0,
+  p_change_password_on_first_use => 'Y',
+  p_first_password_use_occurred  => 'Y',
+  p_allow_app_building_yn        => 'Y',
+  p_allow_sql_workshop_yn        => 'Y',
+  p_allow_team_development_yn    => 'Y',
+  p_allow_access_to_schemas      => '');
+end;
+/
+---------------------------
+-- D G  B L U E P R I N T S
+-- Creating Data Generator Blueprints...
+prompt Check Compatibility...
+begin
+-- This date identifies the minimum version required to import this file.
+wwv_flow_team_api.check_version(p_version_yyyy_mm_dd=>'2010.05.13');
+end;
+/
+ 
+begin wwv_flow.g_import_in_progress := true; wwv_flow.g_user := USER; end; 
+/
+ 
+--
+prompt ...feedback
+--
+begin
+null;
+end;
+/
+--
+prompt ...Issue Templates
+--
+begin
+null;
+end;
+/
+--
+prompt ...Issue Email Prefs
+--
+begin
+null;
+end;
+/
+--
+prompt ...Label Groups
+--
+begin
+null;
+end;
+/
+--
+prompt ...Labels
+--
+begin
+null;
+end;
+/
+--
+prompt ... Milestones
+--
+begin
+null;
+end;
+/
+--
+prompt ... Issues
+--
+begin
+null;
+end;
+/
+--
+prompt ... Issue Attachments
+--
+begin
+null;
+end;
+/
+--
+prompt ... Issues Milestones
+--
+begin
+null;
+end;
+/
+--
+prompt ... Issues Labels
+--
+begin
+null;
+end;
+/
+--
+prompt ... Issues stakeholders
+--
+begin
+null;
+end;
+/
+--
+prompt ... Issues Comments
+--
+begin
+null;
+end;
+/
+--
+prompt ... Issues Events
+--
+begin
+null;
+end;
+/
+--
+prompt ... Issues Notifications
+--
+begin
+null;
+end;
+/
+ 
+prompt ... Extension Links
+ 
+ 
+prompt ... Extension Grants
+ 
+begin
+wwv_flow_imp.import_end(p_auto_install_sup_obj => nvl(wwv_flow_application_install.get_auto_install_sup_obj, false)
+);
+commit;
+end;
+/
+set verify on feedback on define on
+prompt  ...done
